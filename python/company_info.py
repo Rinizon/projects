d45b1bbf470175828fec746e76fa5a93f3211a31
rinizon <rinizon@gmail.com>
2022-03-27 22:03:23 -0500

    new py

--- python/company_info.py
+++ python/company_info.py
@@ -1,10 +1,10 @@
 import yfinance as yf
 
-#define the ticker symbol
+# define the ticker symbol
 tickerSymbol = 'GME'
 
-#get data on this ticker
+# get data on this ticker
 tickerData = yf.Ticker(tickerSymbol)
 
-#info on the company
+# info on the company
 print(tickerData.info)
--- python/stock_data.py
+++ python/stock_data.py
@@ -1,13 +1,13 @@
 import yfinance as yf
 
-#define the ticker symbol
+# define the ticker symbol
 tickerSymbol = 'GME'
 
-#get data on this ticker
+# get data on this ticker
 tickerData = yf.Ticker(tickerSymbol)
 
-#get the historical prices for this ticker
+# get the historical prices for this ticker
 tickerDf = tickerData.history(period='1d', start='2015-1-1', end='2021-7-3')
 
-#see your data
+# see your data
 print(tickerDf)
