5d3e338cd91ade047597e0c704f066a441d1d87e
rinizon <rinizon@gmail.com>
2023-12-07 20:37:16 -0600

    clean

--- python/hello.py
+++
@@ -1,8 +0,0 @@
-print("Welcome to the greeter program")
-name = input("Enter your name: ")
-print("Greetings " + name)
-
-print("Calculaor program")
-first_number = input("first number: ")
-second_number = input("second number: ")
-print(int(first_number) + int(second_number))
