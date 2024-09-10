213077c4b1d8c5ee8f135fa77e4ebb87f257d224
rinizon <rinizon@gmail.com>
2024-09-10 04:33:15 +0000

    api

---
+++ python/redditapi.py
@@ -0,0 +1,65 @@
+import praw
+import pandas as pd
+
+reddit_read_only = praw.Reddit(client_id="K1bJ8sPZLLJxe5pgdgk-Tg",		 # your client id
+							client_secret="CNmfO3Kg3a1z9P2DmrkM5bp7eEfkQQ",	 # your client secret
+							user_agent="Red_Bot")	 # your user agent
+
+
+subreddit = reddit_read_only.subreddit("wallstreetbets")
+
+# Display the name of the Subreddit
+#print("Display Name:", subreddit.display_name)
+
+# Display the title of the Subreddit
+#print("Title:", subreddit.title)
+
+# Display the description of the Subreddit
+#print("Description:", subreddit.description)
+
+# Display top 5 posts on the Subreddit
+# for post in subreddit.hot(limit=5):
+#     print(post.title)
+#     print()
+
+# posts = subreddit.top("month")
+# # Scraping the top posts of the current month
+
+# posts_dict = {"Title": [], "Post Text": [],
+# 			"ID": [], "Score": [],
+# 			"Total Comments": [], "Post URL": []
+# 			}
+
+# for post in posts:
+# 	# Title of each post
+# 	posts_dict["Title"].append(post.title)
+	
+# 	# Text inside a post
+# 	posts_dict["Post Text"].append(post.selftext)
+	
+# 	# Unique ID of each post
+# 	posts_dict["ID"].append(post.id)
+	
+# 	# The score of a post
+# 	posts_dict["Score"].append(post.score)
+	
+# 	# Total number of comments inside the post
+# 	posts_dict["Total Comments"].append(post.num_comments)
+	
+# 	# URL of each post
+# 	posts_dict["Post URL"].append(post.url)
+
+# # Saving the data in a pandas dataframe
+# top_posts = pd.DataFrame(posts_dict)
+# top_posts
+
+# # Saving data to .csv
+# top_posts.to_csv("Top Posts.csv", index=True)
+
+# URL of the post
+url = "https://www.reddit.com/r/IAmA/comments/m8n4vt/\
+im_bill_gates_cochair_of_the_bill_and_melinda/"
+ 
+# Creating a submission object
+submission = reddit_read_only.submission(url=url)
+
