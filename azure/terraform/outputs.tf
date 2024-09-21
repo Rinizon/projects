6245d39ce25ba6b49cc38e8808381c7048baa300
rinizon <rinizon@gmail.com>
2024-09-21 02:24:59 +0000

    vm

--- azure/terraform/outputs.tf
+++ azure/terraform/outputs.tf
@@ -2,10 +2,6 @@ output "resource_group_name" {
   value = azurerm_resource_group.rg.name
 }
 
-output "azurerm_key_vault_name" {
-  value = azurerm_key_vault.vault.name
-}
-
-output "azurerm_key_vault_id" {
-  value = azurerm_key_vault.vault.id
+output "public_ip_address" {
+  value = azurerm_linux_virtual_machine.my_terraform_vm.public_ip_address
 }
