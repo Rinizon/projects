a10b3a306b95949f15d48d0104dd348f22782ddb
rinizon <rinizon@gmail.com>
2024-10-08 17:12:56 -0500

    formatting

--- azure/azvm_tf/tfdeploy.sh
+++ azure/azvm_tf/tfdeploy.sh
@@ -5,5 +5,4 @@ terraform apply -auto-approve
 
 # Output the SSH private key to a text file
 terraform output -raw private_key_data > ~/.ssh/azkey
-
 echo "SSH private key has been saved to ~/.ssh/azkey"
